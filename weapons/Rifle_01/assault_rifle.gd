26a99eeaf1faacd07818bdc392ed5041bea37810
Matheus-Valviesse <matheusvalviesse@gmail.com>
2023-10-26 17:45:14 -0300

    hu scene

--- weapons/Rifle_01/assault_rifle.gd
+++ weapons/Rifle_01/assault_rifle.gd
@@ -1,1 +1,37 @@
-extends Area2D
+extends Weapon
+
+
+@onready var pistol = $"."
+@onready var point = $Bullet_spawn
+
+func _ready():
+	weapon_texture = "res://weapons/Rifle_01/rifle.png"
+	initate()
+	
+func initate():
+	weapon_type = "range"
+	weapon_Style = "multShoot"
+	weapon_name = 'Rifle'
+	bullet_name = "spark"
+	
+	damage = 20
+	ammo = 10
+	maxAmmo = 10
+	bullet_speed = 300
+	
+func weapon_shoot():
+	
+	if can_fire:
+		
+		weapon = pistol
+		bulletSpawn = point
+		shoot(weapon_Style)
+
+
+func create_weapon(damage,ammo,maxAmmo):
+	
+	damage = damage
+	ammo = ammo
+	maxAmmo = maxAmmo
+	
+	
--- weapons/Shoot-gun_01/combat_shoot-gun.gd
+++ weapons/Shoot-gun_01/combat_shoot-gun.gd
@@ -1,1 +1,38 @@
-extends Area2D
+extends Weapon
+
+
+@onready var pistol = $"."
+@onready var point = $Bullet_spawn
+
+func _ready():
+	weapon_texture = "res://weapons/Shoot-gun_01/shoot-gun.png"
+	initate()
+	
+func initate():
+	sprite.texture = load(weapon_texture)
+	weapon_type = "range"
+	weapon_Style = "barrageShoot"
+	weapon_name = 'shoot_gun'
+	bullet_name = "charge"
+	
+	damage = 20
+	ammo = 8
+	maxAmmo = 10
+	bullet_speed = 300
+	
+func weapon_shoot():
+	
+	if can_fire:
+		
+		weapon = pistol
+		bulletSpawn = point
+		shoot(weapon_Style)
+
+
+func create_weapon(damage,ammo,maxAmmo):
+	
+	damage = damage
+	ammo = ammo
+	maxAmmo = maxAmmo
+	
+	
