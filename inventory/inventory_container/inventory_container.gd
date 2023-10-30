3aaa4cc151335f71563a2c7114137d9ab74ff59f
Matheus-Valviesse <matheusvalviesse@gmail.com>
2023-10-30 16:38:57 -0300

    improvement in inventory and item statuses

--- inventory/inventory_container/inventory_container.gd
+++ inventory/inventory_container/inventory_container.gd
@@ -23,27 +23,36 @@ func set_inventory_window(name):
 	
 func insert_item(type,name,image,data):
 
-	inventory.append([name,image,type,data])
+	inventory.append([name,image,type,data.get_path()])
 	
 	if inventory_window == type:
 		set_itens(inventory_window)
-		
+
+func valid_amount(value):
+	print(inventory)
+	print(value)
+	if "amount" in str(value) :  
+		return value
+	else :
+		return 1
+	
 func set_itens(category):
+	
 	var itens = v_container_itens.get_children()
 	var arr_itens
 	
 	arr_itens = inventory.filter(func(item): return item[2] == category)
-
+	
+	
 	for item in itens:
 		item.queue_free()
 
 	var load_item = preload("res://inventory/item/item_background.tscn")
 	
+
 	for i in arr_itens:
 		
-		var item_instantiate = load_item.instantiate()
-		item_instantiate.item_name = i[0]
-		item_instantiate.item_texture = i[1]
-		item_instantiate.item_info= i[3]
+		var item_instantiate =  load_item.instantiate()
+		item_instantiate.set_item(i[0],i[1],i[3],i[2],valid_amount(i[3]))
 		v_container_itens.add_child(item_instantiate)
 		
--- inventory/item/item_background.gd
+++ inventory/item/item_background.gd
@@ -3,15 +3,34 @@ class_name  item
 
 
 @onready var item_icon = $ItemTexture
+@onready var label = $Label
 
-var item_name
-var item_texture
+@onready var item_index = get_index()
+
+var item_name : String
+var item_texture : String
+var item_type : String
 var item_info
+var item_amount = 0
 
 func _ready():
 
 	item_icon.set_texture(load(item_texture))
-
-
+	update_item(item_type)
+	
+func update_item(type):
+	if type == 'rune' or type == 'weapon' or type == 'equipament':
+		label.set_visible(false)
+	else:
+		label.set_visible(false)
+		label.text = item_amount
+		
+	
+func set_item(name,texture,info,type,amount):
+	item_name = name
+	item_texture = texture
+	item_info = info
+	item_type = type
+	item_amount = item_amount + amount
 
 
